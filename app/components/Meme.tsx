0312bf045b7c59ad7d81b3749fe267e9fa6bff39
Emre Ulgac <ulgacemre@gmail.com>
2024-03-15 19:54:14 +0000

    next babel update

--- app/components/Meme.tsx
+++ app/components/Meme.tsx
@@ -14,6 +14,25 @@ export default function Meme(){
     });
 
 
+    {/*
+
+    [
+        {
+            id: "1",
+            location:"Japan",
+            imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+            title: "Mount Fuji",
+            date: "12 Jan, 2021 - 24 Jan, 2021",
+            description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        },
+        {
+            
+        }
+    ]
+
+    */}
+
+
     function handleInputChange(event:any){
        
         const{name,value} = event.target;
